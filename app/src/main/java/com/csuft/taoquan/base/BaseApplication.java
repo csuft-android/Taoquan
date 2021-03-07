eebcba71d1580ad9f18af123cb97bff7b36b4749
yuchao12315 <904798389@qq.com>
2021-03-07 22:45:10 +0800

    修改了布局深度检测工具

--- app/src/main/java/com/csuft/taoquan/base/BaseApplication.java
+++ app/src/main/java/com/csuft/taoquan/base/BaseApplication.java
@@ -3,10 +3,17 @@ package com.csuft.taoquan.base;
 import android.app.Application;
 import android.content.Context;
 
+import com.csuft.detector.PageDetector;
+
 public class BaseApplication extends Application {
 
     private static Context appContext;
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        PageDetector.instance().init(this);
+    }
 
     @Override
     public void onCreate() {
